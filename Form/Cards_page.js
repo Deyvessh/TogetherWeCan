8fd2924a8165d4bc4b26227a527323e844b32676
Deyvessh <deyvesshkumar@gmail.com>
2018-07-15 15:51:18 +0530

    First Project

---
+++ Form/Cards_page.js
@@ -0,0 +1,35 @@
+
+function relocate_helpageindia() {
+     location.href = "https://www.helpageindia.org/";
+} 
+
+
+function relocate_leprasociety() {
+    location.href = "http://leprasociety.org/"
+}
+
+function relocate_sammaan() {
+    location.href = "http://sammaan.org"
+}
+
+function relocate_pratham() {
+    location.href = "http://www.pratham.org"
+}
+
+function relocate_chetna() {
+    location.href = "http://chetnango.org/"
+}
+
+function relocate_goonj() {
+    location.href = "https://goonj.org/"
+}
+
+function relocate_nanhikali() {
+    location.href = "https://www.nanhikali.org/"
+}
+
+function relocate_smilefoundation() {
+    location.href = "https://www.smilefoundationindia.org"
+}
+
+
---
+++ Form/form.js
@@ -0,0 +1,4 @@
+
+function relocate_cardspage() {
+    location.href = "D:\Dropbox\WebD\Together, We_Can\Card\Cards_page.html";
+}
